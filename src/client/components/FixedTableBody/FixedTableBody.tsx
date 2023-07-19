b2dd101ae939a33057444b4500c0d85e7e36d9e3
Dominik <d.walczak.work@gmail.com>
2023-07-19 16:41:55 +0200

    15 Dynamic generation of <tbody> section rows based on API data downloaded from 'value' variable.

--- src/client/components/FixedTableBody/FixedTableBody.tsx
+++ src/client/components/FixedTableBody/FixedTableBody.tsx
@@ -20,7 +20,27 @@ const FixedTableBody: React.FC<FixedTableBodyProps> = ({
     handleMouseLeaveRow
 }) => {
     return (
-        <div>FixedTableBody</div>
+        <tbody>
+            {value?.map((item: CoinData, index: number) => {
+                const logoUrl = `https://s2.coinmarketcap.com/static/img/coins/64x64/${item.id}.png`;
+                const isHovered = index === hoveredItem;
+                const isSelectedStar = index === selectedItem;
+
+                return (
+                    <TableRow
+                        key={item.id}
+                        isHovered={isHovered}
+                        isSelectedStar={isSelectedStar}
+                        logoUrl={logoUrl}
+                        item={item}
+                        index={index}
+                        handleMouseEnterRow={handleMouseEnterRow}
+                        handleMouseLeaveRow={handleMouseLeaveRow}
+                        setSelectedItem={setSelectedItem}
+                    />
+                );
+            })}
+        </tbody>
     )
 }
 
