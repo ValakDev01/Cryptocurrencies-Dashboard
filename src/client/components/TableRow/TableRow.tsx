53f72a28ea05f77c394aa82ea5edd6ed1c6a6765
Dominik <d.walczak.work@gmail.com>
2023-07-19 16:53:02 +0200

    17 Adding a <tr> row with three <td> cells. The first contains the star <FaRegStar /> component and additionally a dynamically generated <div> window with a message. The second cell contains the 'index' parameter increased by 1. The last cell stores the coin's image, its name and symbol.

--- src/client/components/TableRow/TableRow.tsx
+++ src/client/components/TableRow/TableRow.tsx
@@ -25,7 +25,44 @@ const TableRow: React.FC<TableRowProps> = ({
     setSelectedItem
 }) => {
     return (
-        <div>TableRow</div>
+        <tr 
+            className={isHovered ? "hovered-row" : ""}
+            onMouseEnter={() => handleMouseEnterRow(index)}
+            onMouseLeave={handleMouseLeaveRow}
+        >
+
+            <td className="firstTd">
+                <FaRegStar
+                    style={{color: isSelectedStar ? 'gold' : 'gray', cursor: 'pointer'}}
+                    onMouseEnter={() => setSelectedItem(index)}
+                    onMouseLeave={() => setSelectedItem(null)}
+                />
+
+                {isSelectedStar && (
+                    <div className="notificationStar">
+                        <div className="triangle"></div>
+                        <div className="content">Add to Main Watchlist and follow coin</div>
+                    </div>
+                )}
+            </td>
+
+            <td style={{ textAlign: 'left', verticalAlign: 'middle' }}>
+                {index + 1}
+            </td>
+
+            <td style={{ textAlign: 'left' }}>
+                <div className="divData">
+                    <img src={logoUrl} alt='' className="imgStyle" />
+                    <span className="firstSpan">
+                        {item.name}
+                    </span>
+
+                    <span className="secondSpan">
+                        {item.symbol}
+                    </span>
+                </div>
+            </td>
+        </tr>
     )
 }
 
