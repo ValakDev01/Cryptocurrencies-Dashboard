c27a7248bc16f3d32e75f76cefaf4d9756aa840a
Dominik <d.walczak.work@gmail.com>
2023-07-19 17:54:18 +0200

    30 Adding a method called scrollToTop(). Adding a <div> tag to the JSX section which, when clicked, will run the above method. This tag contains an additional arrow component called <IoMdArrowRoundUp />.

--- src/client/components/ScrollToTopButton/ScrollToTopButton.tsx
+++ src/client/components/ScrollToTopButton/ScrollToTopButton.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from "react";
+import { IoMdArrowRoundUp } from 'react-icons/io';
+import './ScrollToTopButton.css';
 
-const ScrollToTopButton = () => {
+const ScrollToTopButton: React.FC = () => {
+    const scrollToTop = () => {
+        const tableContainer = document.querySelector(".table-container") as HTMLDivElement;
+
+        tableContainer.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     return (
-        <div>ScrollToTopButton</div>
+        <div className="scroll-to-top-button" onClick={scrollToTop}>
+            <IoMdArrowRoundUp />
+        </div>
     )
 }
 
