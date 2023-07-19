fb6b095bdf6c6101fb2bdffeff497640fa95b285
Dominik <d.walczak.work@gmail.com>
2023-07-19 16:39:11 +0200

    14 Adding types interface for attributes passed from the <FixedTable /> component to the <FixedTableBody /> component.

--- src/client/components/FixedTableBody/FixedTableBody.tsx
+++ src/client/components/FixedTableBody/FixedTableBody.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React from "react";
+import TableRow from "../TableRow/TableRow";
+import { CoinData } from "../../../App";
 
-const FixedTableBody = () => {
+interface FixedTableBodyProps {
+    value: CoinData[];
+    selectedItem: number | null;
+    setSelectedItem: React.Dispatch<React.SetStateAction<number | null>>;
+    hoveredItem: number | null;
+    handleMouseEnterRow: (index: number) => void;
+    handleMouseLeaveRow: () => void;
+}
+
+const FixedTableBody: React.FC<FixedTableBodyProps> = ({
+    value,
+    selectedItem,
+    setSelectedItem,
+    hoveredItem,
+    handleMouseEnterRow,
+    handleMouseLeaveRow
+}) => {
     return (
         <div>FixedTableBody</div>
     )
