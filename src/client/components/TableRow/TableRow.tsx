cece76b2b033160b261d8f698e4aa1a66d42e9e5
Dominik <d.walczak.work@gmail.com>
2023-07-19 16:45:47 +0200

    16 Adding types interface for attributes passed from the <FixedTableBody /> component to the <TableRow /> component.

--- src/client/components/TableRow/TableRow.tsx
+++ src/client/components/TableRow/TableRow.tsx
@@ -1,6 +1,29 @@
 import React from 'react'
+import { FaRegStar } from 'react-icons/fa';
+import { CoinData } from "../../../App";
+import './TableRow.css';
 
-const TableRow = () => {
+interface TableRowProps {
+    isHovered: boolean;
+    isSelectedStar: boolean;
+    logoUrl: string;
+    item: CoinData;
+    index: number;
+    handleMouseEnterRow: (index: number) => void;
+    handleMouseLeaveRow: () => void;
+    setSelectedItem: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+const TableRow: React.FC<TableRowProps> = ({
+    isHovered,
+    isSelectedStar,
+    logoUrl,
+    item,
+    index,
+    handleMouseEnterRow,
+    handleMouseLeaveRow,
+    setSelectedItem
+}) => {
     return (
         <div>TableRow</div>
     )
