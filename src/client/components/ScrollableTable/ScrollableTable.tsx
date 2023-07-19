d7e16de167a4e758001cdc87dae0d4c34f42546e
Dominik <d.walczak.work@gmail.com>
2023-07-19 17:09:50 +0200

    19 Adding types interface for attributes passed from the <App /> component to the <ScrollableTable /> component.

--- src/client/components/ScrollableTable/ScrollableTable.tsx
+++ src/client/components/ScrollableTable/ScrollableTable.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React from 'react';
+import ScrollableTableHead from "../ScrollableTableHead/ScrollableTableHead";
+import ScrollableTableBody from "../ScrollableTableBody/ScrollableTableBody";
+import { CoinData } from "../../../App";
+import './ScrollableTable.css';
 
-const ScrollableTable = () => {
+interface ScrollableTableProps {
+    value: CoinData[];
+    hoveredItem: number | null;
+    handleMouseEnterRow: (index: number) => void;
+    handleMouseLeaveRow: () => void;
+}
+
+const ScrollableTable: React.FC<ScrollableTableProps> = ({
+    value,
+    hoveredItem,
+    handleMouseEnterRow,
+    handleMouseLeaveRow
+}) => {
     return (
         <div>ScrollableTable</div>
     )
